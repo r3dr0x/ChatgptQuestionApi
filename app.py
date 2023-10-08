4be08a03095274a834661d0467bad32fe8a7f93f
{Ömer} <77547392+r3dr0x@users.noreply.github.com>
2023-10-08 19:02:39 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import openai
+
+openai.api_key = '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'
+
+app = Flask(__name__)
+
+@app.route('/chat', methods=['GET'])
+def chat():
+    message = request.args.get('message')
+    
+    if message is None:
+        return jsonify({'error': 'No message provided'}), 400
+
+    messages = [
+        {"role": "system", "content": "You are a intelligent assistant."},
+        {"role": "user", "content": message}
+    ]
+
+    chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", 
+        messages=messages
+    )
+
+    reply = chat.choices[0].message.content
+
+    response = {
+        "result": {
+            "query": message,
+            "reply": reply
+        }
+    }
+
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
